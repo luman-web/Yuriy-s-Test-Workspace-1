c4dd7a10afb7b46e04744296de44dd414eb4b3da
luman-web <luman-web@yandex.ru>
2025-08-29 14:26:01 +0000

    (emitter: lumanweb1@gmail.com) commit 1

---
+++ components/u-sample-component/compoenentCode.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    sample
+  </div>
+</template>
+
+<script>
+module.exports = {
+  props: {},
+  data() {
+    return {}
+  },
+  beforeCreate() {},
+  created() {},
+  beforeMount() {},
+  mounted() {},
+  beforeUpdate() {},
+  updated() {},
+  beforeDestroy() {},
+  destroyed() {},
+}
+</script>
+
+<style>
+</style>
